64ec66fd05c71bfcc416759b985ca9e7568871eb
Crystal <kangna@jushiwangedu.com>
2019-05-09 16:29:25 +0800

    新增cookie处理方法 update by Crystal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = {
+let jswToolkit = {
     /**
      * 手机号校验
      * @param phone  需要校验的手机号
@@ -84,6 +84,22 @@ module.exports = {
         return obj;
     },
     /**
+     * 密码再次确认
+     * @param password  密码
+     * @param confirmPassword  确认密码
+     * @returns {Object}
+     */
+    confirmPassword (password, confirmPassword) {
+        if (password !== confirmPassword) {
+            obj.code = 40003;
+            obj.msg = '两次输入密码不一致';
+        } else {
+            obj.code = 200;
+            obj.msg = '校验成功';
+        }
+        return obj;
+    },
+    /**
      * 检测客户端类型
      * @returns 0:web or 1:h5 or 2:wx
      */
@@ -94,5 +110,52 @@ module.exports = {
         } else {
             return 0;
         }
+    },
+    /**
+    * 读cookie
+    * @param c_name  cookie的key值
+    * @returns cookie的value值
+    */
+    getCookie (c_name) {
+        if (document.cookie.length > 0) {
+            let c_start = document.cookie.indexOf(c_name + "=")
+            if (c_start != -1) {
+                c_start = c_start + c_name.length + 1
+                let c_end = document.cookie.indexOf(";", c_start)
+                if (c_end == -1) c_end = document.cookie.length
+                return unescape(document.cookie.substring(c_start, c_end))
+            }
+        }
+        return "";
+    },
+    /**
+    * 存cookie
+    * @param c_name  cookie的key值
+    * @param value  cookie的value值
+    * @param expire  过期时间 默认100年
+    */
+    setCookie (c_name, value, expire = 60 * 60 * 24 * 366 * 100) {
+        var date = new Date()
+        date.setSeconds(date.getSeconds() + expire)    //获取秒数 date.getSetSeconds()  + （24h*60） * 60秒 * 天数  
+        document.cookie = c_name + "=" + escape(value) + "; expires=" + date.toGMTString()  //toGMTString 转成格林威治时间 (GMT); 电脑端的中国时间 = 对应的GMT+8之后的
+    },
+    /**
+     * 删除cookie，原理就是把原有的cookie的value设置为‘’
+     * @param c_name  cookie的key值
+     */
+    delCookie (c_name) {
+        setCookie(c_name, "", -1)
+    },
+    /**
+     * 清除所有cookie,原理就是循环获取所有已存储的cookie的key值，根据key值将value设置为‘’
+     */
+    clearCookie () {
+        let arr = document.cookie.split(';');
+        if (arr.length > 0) {
+            for (let i = 0; i < arr.length; i++) {
+                setCookie(arr[i].split('=')[0], "", -1)
+            }
+        }
     }
 }
+module.exports = jswToolkit;
